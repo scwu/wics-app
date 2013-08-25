6582969c3666a4fa24102ee7d9793b2b2a7fd383
Clara Wu <s.clara.wu@gmail.com>
2013-08-25 16:15:42 -0400

    update settings, shot in the dar

--- mysite/settings.py
+++ mysite/settings.py
@@ -3,7 +3,7 @@
 
 import sys, os #HP
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -69,7 +69,7 @@ MEDIA_URL = '/media/' #HP
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -86,6 +86,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'mysite/static'),
 )
 
 # List of finder classes that know how to find static files in
